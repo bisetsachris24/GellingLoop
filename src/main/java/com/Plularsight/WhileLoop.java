28c7938bcef591d0989f62788771e08860e590c3
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-04-17 14:09:32 -0500

    Initial commit

--- src/main/java/com/Plularsight/WhileLoop.java
+++ src/main/java/com/Plularsight/WhileLoop.java
@@ -3,10 +3,13 @@ package com.Plularsight;
 public class WhileLoop {
     public static void main(String[] args) {
 
+        // integer count
         int i =1;
+        // creating while loop
         while (i <= 5){
-
+    // printing our
             System.out.println(" I love Java");
+            //adding one
             i++;
         }
     }
