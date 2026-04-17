949301b0621de2bfdf9d7511d43059de29ca7aaa
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-04-17 14:28:38 -0500

    Initial commit

--- src/main/java/com/Plularsight/Dice.java
+++ src/main/java/com/Plularsight/Dice.java
@@ -5,8 +5,6 @@ public class Dice {
     //roll method that returns a random number between 6 and 1
     public int roll() {
 
-        int randomNumber = (int)(Math.random() * 6) + 1;
-
-        return randomNumber;
+        return (int)(Math.random() * 6) + 1;
     }
 }
